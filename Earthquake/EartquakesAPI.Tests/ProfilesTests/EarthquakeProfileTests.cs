ba54950dd801e877d165d95a8e87c56f7eeb47a8
Nicoleta Hermeneanu <93427987+nicoletahrm@users.noreply.github.com>
2023-03-22 17:44:22 +0200

    Finished AutoMapper testing

--- Earthquake/EartquakesAPI.Tests/ProfilesTests/EarthquakeProfileTests.cs
+++ Earthquake/EartquakesAPI.Tests/ProfilesTests/EarthquakeProfileTests.cs
@@ -8,24 +8,88 @@ using AutoMapper;
 using Xunit;
 using Shouldly;
 using Earthquake.API.Models;
+using Earthquake;
+using Earthquake.API.Profiles;
 
 namespace EartquakesAPI.Tests.ProfilesTests
 {
     public class EarthquakeProfileTests
     {
         [Fact]
-        public void Test_CreateMap_Earthquake_To_EarthquakeResponse()
+        public void Test_CreateMap_Feature_To_EarthquakeResponse()
         {
             var target = GetTarget();
 
-            var earthquake = new Earthquake(target);
+            Feature earthquakeFeature = new Feature()
+            {
+                Properties = new Properties()
+                {
+                    Magnitude = (decimal)4.9,
+                    Place = "3 km WSW of Arcani, Romania",
+                    TimeMs = 1679320935362,
+                    Updated = 1679483643771,
+                    Url = "\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/us7000jlf8\",\"detail\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us7000jlf8&format=geojson\"",
+                    Tz = null,
+                },
+                Geometry = new Geometry()
+                {
+                    Type = "Point",
+                    Coordinates = new List<double>()
+                    {
+                        23.0866,
+                        45.0762,
+                        10
+                    }
+                }
+            };
 
-            var result = target.Map<EarthquakeResponse>(earthquake);
+            var result = target.Map<EarthquakeEntity>(earthquakeFeature);
 
+            result.Properties.Time.ShouldBe(earthquakeFeature.Properties.Time);
+            result.Properties.Type.ShouldBe(earthquakeFeature.Properties.Type);
+            result.Geometry.Coordinates.ShouldBe(earthquakeFeature.Geometry.Coordinates);
         }
+
+        [Fact]
+        public void Test_CreateMap_EarthquakeEntity_To_EarthquakeResponse()
+        {
+            var target = GetTarget();
+
+            EarthquakeEntity earthquakEntity = new EarthquakeEntity()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Properties = new Properties()
+                {
+                    Magnitude = (decimal)4.9,
+                    Place = "3 km WSW of Arcani, Romania",
+                    TimeMs = 1679320935362,
+                    Updated = 1679483643771,
+                    Url = "\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/us7000jlf8\",\"detail\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us7000jlf8&format=geojson\"",
+                    Tz = null,
+                },
+                Geometry = new Geometry()
+                {
+                    Type = "Point",
+                    Coordinates = new List<double>()
+                    {
+                        23.0866,
+                        45.0762,
+                        10
+                    }
+                }
+            };
+
+            var result = target.Map<EarthquakeEntity>(earthquakEntity);
+
+            result.Id.ShouldBe(earthquakEntity.Id);
+            result.Properties.Magnitude.ShouldBe(earthquakEntity.Properties.Magnitude);
+            result.Properties.Type.ShouldBe(earthquakEntity.Properties.Type);
+            result.Geometry.Coordinates.ShouldBe(earthquakEntity.Geometry.Coordinates);
+        }
+
         private IMapper GetTarget()
         {
-            var config = new MapperConfiguration(cfg => cfg.AddProfile<Earthquake.API.Profiles>());
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<EarthquakeProfile>());
             var mapper = config.CreateMapper();
 
             return mapper;
