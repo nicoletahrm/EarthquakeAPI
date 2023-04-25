d3ffa7494fe3fc50ae03febdb5c561f9fdbafe9a
Nicoleta Hermeneanu <93427987+nicoletahrm@users.noreply.github.com>
2023-04-25 12:18:46 +0300

    Made fields required

--- EarthquakeAngular/src/app/earthquakes-by-params/earthquakes-by-params.component.ts
+++ EarthquakeAngular/src/app/earthquakes-by-params/earthquakes-by-params.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { EarthquakeResponse } from '../models/earthquake-response';
 import { EarthquakeService } from '../services/earthquake.service';
 import { EarthquakeRequest } from '../models/earthquakes-request';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject, filter, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-earthquakes-by-params',
@@ -11,13 +13,38 @@ import { EarthquakeRequest } from '../models/earthquakes-request';
 export class EarthquakesByParamsComponent {
   pageTitle = 'Earthquakes by params';
 
-  earthquakesToDisplay: EarthquakeResponse[] = [];
+  earthquakesToDisplay: EarthquakeResponse[];
   errorMessage: any;
-  
-  start: Date = new Date();
-  end: Date = new Date();
-  maxMagnitude: number = 5;
-  orderBy: string = 'time';
+  earthquakeForm: FormGroup;
+  private unsubscribe$ = new Subject();
+
+  initializeRequest() {
+
+    this.earthquakeForm = this.fb.group({
+      start: [null, [Validators.required]],
+      end: [null, [Validators.required]],
+      magnitude: [null],
+      orderBy: [null],
+    });
+
+    this.earthquakeForm.valueChanges
+      .pipe(
+        filter(() => !this.earthquakeForm.pristine),
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe(() => {
+        const startTime = this.getSartTime?.value;
+        const endTime = this.getEndTime?.value;
+
+        if (startTime && endTime) {
+          this.earthquakeRequest.startTime = new Date(startTime);
+          this.earthquakeRequest.endTime = new Date(endTime);
+        }
+
+        this.earthquakeRequest.maxMagnitude = this.getMagnitude?.value;
+        this.earthquakeRequest.orderBy = this.getOrderBy?.value;
+      });
+  }
 
   earthquakeRequest: EarthquakeRequest = {
     startTime: new Date(),
@@ -26,27 +53,49 @@ export class EarthquakesByParamsComponent {
     orderBy: '',
   };
 
-  constructor(private earthquakeService: EarthquakeService) {}
+  constructor(
+    private earthquakeService: EarthquakeService,
+    private fb: FormBuilder
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initializeRequest();
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next;
+  }
 
   getEarthquakes() {
-    this.earthquakeRequest.startTime = this.start;
-    this.earthquakeRequest.endTime = this.end;
-    this.earthquakeRequest.maxMagnitude = this.maxMagnitude;
-    this.earthquakeRequest.orderBy = this.orderBy;
-
-    this.earthquakeService
-      .getEarthquakesByParams(this.earthquakeRequest)
-      .subscribe({
-        next: (earthquakes) => {
-          this.earthquakesToDisplay = earthquakes;
-        },
-        error: (err) => (this.errorMessage = err),
-      });
+    if (this.earthquakeForm.valid) {
+      this.earthquakeService
+        .getEarthquakesByParams(this.earthquakeRequest)
+        .subscribe({
+          next: (earthquakes) => {
+            this.earthquakesToDisplay = earthquakes;
+          },
+          error: (err) => (this.errorMessage = err),
+        });
+    }
   }
 
   clickEarthquake(earthquake: EarthquakeResponse) {
     this.earthquakeService.updateEarthquake(earthquake);
+  }
+
+  get getSartTime() {
+    return this.earthquakeForm.get('start');
+  }
+
+  get getEndTime() {
+    return this.earthquakeForm.get('end');
+  }
+
+  get getMagnitude() {
+    return this.earthquakeForm.get('magntiude');
+  }
+
+  get getOrderBy() {
+    return this.earthquakeForm.get('orderBy');
   }
 }
